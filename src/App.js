5aedb6892865e87966e9bb0621b78257e03d0874
iamperfect927 <djielaparfait99@gmail.com>
2023-07-26 04:02:56 +0100

    routes to all pages created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
 
 function App() {
   return (
-    <div >
-      <h1>hello world</h1>
+    // fragments in js
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
         
-    </div>
+    </>
   );
 }
 
